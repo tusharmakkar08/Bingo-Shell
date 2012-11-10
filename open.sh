29d7a25b132882e1c7c7aa7846dd919721e419e2
Tushar <tushar@ubuntu.ubuntu-domain>
2012-11-10 15:41:04 +0530

    Small changes like Increasing the window size , making everything in bold etc

--- open.sh
+++ open.sh
@@ -1,2 +1,2 @@
-gnome-terminal  --window-with-profile=BingoShell -x /home/tushar/Unix\ project/projectbingo.sh
+gnome-terminal --geometry=240X240+350+350  --window-with-profile=BingoShell -x /home/tushar/Unix\ project/projectbingo.sh
 
